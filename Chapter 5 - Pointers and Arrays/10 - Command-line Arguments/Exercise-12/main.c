5e7616fee14a2067d47909e4b511110d639402e0
Joe <37711916+JoeWrieden@users.noreply.github.com>
2019-10-06 14:58:38 +0100

    Update main.c

--- Chapter 5 - Pointers and Arrays/10 - Command-line Arguments/Exercise-12/main.c
+++ Chapter 5 - Pointers and Arrays/10 - Command-line Arguments/Exercise-12/main.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void tailx(char *[], int, int);
-
+void tailx(char *[], int);
+int numfind(char *[], int);
 
 int main(int argc, char *argv[]) {
-    tailx(argv, 10, argc);
+    int n = numfind(argv, argc);
+
+    int c = argc - n;
+    tailx(argv, c);
     return 0;
 }
 
-void tailx(char *s[], int n, int c) {
+int numfind(char *s[], int p){
+    while (*s){
+        if (strcmp(*s,"-n") == 0){
+            s++;
+            int c = atoi(*s);
+            if (c > (p - 2)) {
+                printf("Too long defaulting length to 10\n");
+                return 10;
+            }
+            return c;
+            }
+        else
+            s++;
+    }
+    return 10;
+}
+
+void tailx(char *s[], int increment) {
     int i;
-    int increment = c - n;
 
     for (i = 0; i < increment; ++i)
         s++;
