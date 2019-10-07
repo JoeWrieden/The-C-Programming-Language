5482c964ca6c5adde94e52f5f1140ae9a871e663
Joe <37711916+JoeWrieden@users.noreply.github.com>
2019-10-08 00:17:53 +0100

    Update StrCompV2

---
+++ Chapter 5 - Pointers and Arrays/11 - Pointers to Functions/StrCompV2/StrComp.h
@@ -0,0 +1,9 @@
+#define MAXLINES 5000
+
+int readlines(char *lineptr[], int nlines);
+int getlinex(char *, int);
+void writelines(char *lineptr[], int nlines);
+
+void swap(void *v[], int, int);
+void qsortx(void *lineptr[], int left, int right, int (*comp)(void *, void *));
+int numcmp(char *, char *);
---
+++ Chapter 5 - Pointers and Arrays/11 - Pointers to Functions/StrCompV2/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "StrComp.h"
+
+char *lineptr[MAXLINES];
+
+
+/* sort input lines */
+int main(int argc, char *argv[]) {
+    int nlines;
+    int numeric = 0;
+
+    if (argc > 1 && strcmp(argv[1], "-n") == 0)
+        numeric = 1;
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+        qsortx((void **) lineptr, 0, nlines - 1, (int (*)(void *, void *)) (numeric ? numcmp : strcmp));
+        writelines(lineptr, nlines);
+        return 0;
+    } else {
+        printf("input too big to sort\n");
+        return 1;
+    }
+}
+
+void qsortx(void *v[], int left, int right, int (*comp)(void *, void *)){
+    int i, last;
+
+    if (left >= right)
+        return;
+    swap(v, left, (left + right)/2);
+    last = left;
+    for (i = left+1; i <= right; i++)
+        if ((*comp)(v[i], v[left]) < 0)
+            swap(v, ++last, i);
+    swap(v, left, last);
+    qsortx(v, left, last-1, comp);
+    qsortx(v, last+1, right, comp);
+}
+
+int numcmp(char *s1, char *s2){
+    double v1, v2;
+
+    v1 = atof(s1);
+    v2 = atof(s2);
+    if (v1 < v2)
+        return -1;
+    else if (v1 > v2)
+        return 1;
+    else
+        return 0;
+}
+
+void swap(void *v[], int i, int j){
+    void *temp;
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
